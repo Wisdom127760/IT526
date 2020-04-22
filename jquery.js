37c132336f9c6cf6ba4d5a37f62dd6d057e68ee8
Wisdom <31096761+Wisdom127760@users.noreply.github.com>
2020-04-22 03:00:46 +0300

    Update jquery.js

---
+++ jquery.js
@@ -0,0 +1,39 @@
+
+var Frequency = (function () {
+  function Frequency() {
+  }
+  Frequency.main = function (args) {
+      var str = prompt("Please enter your message", "Here!");
+      var freq = (function (s) { var a = []; while (s-- > 0)
+          a.push(0); return a; })(str.length);
+      var i;
+      var j;
+      var string = (str).split('');
+      for (i = 0; i < str.length; i++) {
+          {
+              freq[i] = 1;
+              for (j = i + 1; j < str.length; j++) {
+                  {
+                      if ((function (c) { return c.charCodeAt == null ? c : c.charCodeAt(0); })(string[i]) == (function (c) { return c.charCodeAt == null ? c : c.charCodeAt(0); })(string[j])) {
+                          freq[i]++;
+                          string[j] = '0';
+                      }
+                  }
+                  ;
+              }
+          }
+          ;
+      }
+      console.info("Letters in English:");
+      for (i = 0; i < freq.length; i++) {
+          {
+              if ((function (c) { return c.charCodeAt == null ? c : c.charCodeAt(0); })(string[i]) != ' '.charCodeAt(0) && (function (c) { return c.charCodeAt == null ? c : c.charCodeAt(0); })(string[i]) != '0'.charCodeAt(0))
+                  console.info(string[i] + "-" + freq[i]);
+          }
+          ;
+      }
+  };
+  return Frequency;
+}());
+Frequency["__class"] = "Frequency";
+Frequency.main(null);

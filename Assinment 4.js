4ad0d236a7366f71a0639802913151fd9af80e98
Wisdom127760 <31096761+Wisdom127760@users.noreply.github.com>
2020-05-10 17:05:45 +0300

    Create Assinment 4.js

---
+++ Assinment 4.js
@@ -0,0 +1,32 @@
+function findPrime($input) {
+	
+	function isPrime($x) {
+	 
+	  var $temp = new Array($x);
+  
+	  for (var $i = 2; $i < $x; $i++) {
+		
+		if ($x % $i == 0) {
+		
+		  $temp[$i] = false;
+		} else {
+		  
+		  $temp[$i] = true;
+		}
+	  }
+  
+	  for (var $y = 2; $y < $x; $y++) {
+		
+		if ($temp[$y] == false) {
+		  
+		  return false;
+		}else
+	  
+	  return true;
+
+	  
+	}
+	var $count = 0;
+	$count++;
+  }
+}

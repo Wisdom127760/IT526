b878717a6bcf13f679cb261df1228d8d4eee90b8
Wisdom127760 <31096761+Wisdom127760@users.noreply.github.com>
2020-05-23 02:58:07 +0300

    Update Assinment 5.js

--- Assinment 5.js
+++ Assinment 5.js
@@ -5,4 +5,14 @@ var sieve_of_eratosthenes = function(num) {
 
     var  upperLimit = Math.sqrt(num) /* convert the user input to upper limit. */, array = [], output = []; 
 
+    for (var x = 0; x < n; x++) {
+        array.push(true);
+    }
+    for (var i = 2; i <= upperLimit; i++) {
+        if (array[i]) {
+            for (var j = i * i; j < n; j += i) {
+                array[j] = false;
+            }
+        }
+    }
 }

7a5469f7b0463b2701d2592731c95ee46842965e
Wisdom127760 <31096761+Wisdom127760@users.noreply.github.com>
2020-05-23 01:08:36 +0300

    Create Assinment 5.js

---
+++ Assinment 5.js
@@ -0,0 +1,8 @@
+//using the Sieve of Eratosthenes algorithm
+
+//let the prime number start from 2 
+var sieve_of_eratosthenes = function(num) {
+
+    var  upperLimit = Math.sqrt(num) /* convert the user input to upper limit. */, array = [], output = []; 
+
+}
